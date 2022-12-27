1f664f7213dd772d05c56c768b87ccc07e8d1db4
Adeposi <adeposi60@gmail.com>
2022-12-27 12:23:46 +0400

    Final fixes to codebase

--- index.js
+++ index.js
@@ -3,28 +3,9 @@ var fs = require('fs')
 const { readFileSync, promises: fsPromises } = require('fs')
 const { callbackify } = require('util')
 
-// let filePath = './design-modern-electrician/web.data.Site.site.json'
-// let writeFilePath = './design-modern-electrician/web.data.Site.site.fixed.json'
 const source = './sample_templates'
 
-// let json = require(filePath)
-
-// json.folder?.items.map((item, index) => {
-//     if (item.url === "home") {
-//         item.hidden = false
-//     } else if (!item.hasOwnProperty('url')) {
-//         item.hidden = item.hidden
-//     } else {
-//         item.hidden = true
-//     }
-//     return (
-//         console.log(item.hidden)
-//     )
-// })
-
-// console.log(json.folder.items);
-// fs.writeFileSync(writeFilePath, JSON.stringify(json))
-
+//function to retreive all directories in templates folder
 const getDirectories = source =>
     fs.readdirSync(source, { withFileTypes: true })
         .filter(dirent => dirent.isDirectory())
@@ -32,6 +13,7 @@ const getDirectories = source =>
 
 console.log(getDirectories(source))
 
+//function to manipulate files
 function fixFiles(src){
     for (let i = 0; i < src.length; i++) {
         let singleDirectory = src[i]
@@ -54,6 +36,8 @@ function fixFiles(src){
         })
     
         console.log(json.folder.items);
+
+        //writing files back to json
         fs.writeFileSync(writeFilePath, JSON.stringify(json))
     }
 }
