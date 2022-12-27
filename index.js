d3f640d1cf79e07df9c6be9f320fe898b3279308
Adeposi <adeposi60@gmail.com>
2022-12-27 13:04:09 +0400

    Fixed write file erros

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const { callbackify } = require('util')
 
 const source = './sample_templates'
 
+// let filePath = './design-modern-electrician/web.data.Site.site.json'
+// let writeFilePath = './design-modern-electrician/web.data.Site.site.fixed.json'
+
+// let json = require(filePath)
+
+// json.folder?.items.map((item, index) => {
+//     if (item.url === "home") {
+//         item.hidden = false
+//     } else if (!item.hasOwnProperty('url')) {
+//         item.hidden = item.hidden
+//     } else {
+//         item.hidden = true
+//     }
+//     return (
+//         console.log(item.hidden)
+//     )
+// })
+
+// console.log(json.folder.items);
+// fs.writeFileSync(writeFilePath, JSON.stringify(json))
+
 //function to retreive all directories in templates folder
 const getDirectories = source =>
     fs.readdirSync(source, { withFileTypes: true })
@@ -18,7 +39,8 @@ function fixFiles(src){
     for (let i = 0; i < src.length; i++) {
         let singleDirectory = src[i]
         let filePath = `./sample_templates/${singleDirectory}/web.data.Site.site.json`
-        let writeFilePath = `./sample_templates/${singleDirectory}/web.data.Site.site.fixed.json`
+        let writeFilePath = `./fixed/${singleDirectory}/`
+        let writeFile = `./fixed/${singleDirectory}/web.data.Site.site.json`
         
      
         let json = require(filePath)
@@ -38,7 +60,8 @@ function fixFiles(src){
         console.log(json.folder.items);
 
         //writing files back to json
-        fs.writeFileSync(writeFilePath, JSON.stringify(json))
+        fs.mkdirSync(writeFilePath)
+        fs.writeFileSync(writeFile, JSON.stringify(json))
     }
 }
 
